73d0110ef0d5644b8340a43cf9bd416015fc6dbb
MykeNuLeng <linford.mike@gmail.com>
2022-02-23 07:01:54 +0000

    Severely broken, but refactored

--- Timetable/Program.cs
+++ Timetable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Timetable.UserInput;
 
 namespace Timetable
 {
@@ -6,52 +7,12 @@ namespace Timetable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many students do you have?");
-            string numberOfStudents = Console.ReadLine();
-            bool isInt = int.TryParse(numberOfStudents, out int studentNumber);
+            int studentNumber = UserInput.NumberOfStudents();
 
-            if (isInt && studentNumber > 0)
-            {
-                Console.WriteLine(studentNumber);
-            } else
-            {
-                Console.WriteLine("That isn't a valid number");
-            }
+            string[] studentArr = UserIput.StudentArray(studentNumber);
 
-            string[] studentArr = new string[studentNumber];
+            Hashtable studentsAvailability = UserInput.StudentHash(studentArr);
 
-            for (int i = 0; i < studentArr.Length; i++)
-            {
-                Console.WriteLine("Please enter the name of student number " + (i + 1));
-                studentArr[i] = Console.ReadLine();
-            }
-
-            Hashtable studentsAvailability = new Hashtable(studentNumber);
-
-            for (int i = 0; i < studentArr.Length; i++)
-            {
-                bool isStillAvailable = true;
-
-                while (isStillAvailable)
-                {
-                    Console.WriteLine("Please enter the availability of " + studentArr[i] + ". Or type N for no more availability");
-                    string availability = Console.ReadLine();
-
-                    if (availability == "N")
-                    {
-                        isStillAvailable = false;
-                    } else
-                    {
-                        string[] times = availability.Split(",");
-                        studentsAvailability.Add(studentArr[i], times);
-                    }
-                }  
-            }
-
-            foreach (string student in studentsAvailability.Keys)
-            {
-                Console.WriteLine(student); 
-            }
         }
     }
 }
---
+++ Timetable/UserInput/NumberOfStudents.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Timetable.UserInput
+{
+    public class UserInput
+    {
+
+        public static int NumberOfStudents()
+        {
+            Console.WriteLine("How many students do you have?");
+            string numberOfStudents = Console.ReadLine();
+            bool isInt = int.TryParse(numberOfStudents, out int studentNumber);
+
+            while (!isInt)
+            {
+                Console.WriteLine("Please enter a valid number of students");
+                numberOfStudents = Console.ReadLine();
+                isInt = int.TryParse(numberOfStudents, out studentNumber);
+            }
+
+            return studentNumber;
+        }
+
+        public static string[] StudentArray(int studentNumber)
+        {
+
+            string[] studentArr = new string[studentNumber];
+
+            for (int i = 0; i < studentNumber; i++)
+            {
+                Console.WriteLine("Please enter the name of student number " + (i + 1));
+                studentArr[i] = Console.ReadLine();
+            }
+            return studentArr;
+        }
+
+
+        public static Hashtable StudentHash(string[] studentArr)
+        {
+            int studentNumber = studentArr.Length;
+
+            Hashtable studentsAvailability = new Hashtable(studentNumber);
+
+            for (int i = 0; i < studentNumber; i++)
+            { 
+                Console.WriteLine("Please enter the availability of " + studentArr[i]);
+                string availability = Console.ReadLine();
+
+                string[] times = availability.Split(", ");
+                studentsAvailability.Add(studentArr[i], times);
+            }
+        }
+    }
+}
